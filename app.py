b20bb69f0d7e2273039e945021e99ff79211320f
rajaack <jackyrajemison@gmail.com>
2025-03-29 22:41:01 +0100

    add true question

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import io
 
 st.write("""
 SQL SRS
 Spaced Repetition System SQL practice
 """)
+
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
+
+solution = duckdb.sql(answer).df()
 
 with st.sidebar:
     option = st.selectbox(
@@ -19,11 +43,21 @@ with st.sidebar:
 
 data = {"a": [1,2,3], "b": [4,5,6]}
 
-df = pd.DataFrame(data)
-
-st.write("Dataframe initial :")
-st.dataframe(df)
-query = st.text_area(label="entrez votre input")
-
+st.header("enter your code:")
+query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
-    st.write(duckdb.sql(query).df())
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
+
+with tab3:
+    st.write(answer)
