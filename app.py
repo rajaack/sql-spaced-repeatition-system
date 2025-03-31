f7550070314182db9d1ee9ceecb7cd7fedbd848b
rajaack <jackyrajemison@gmail.com>
2025-03-31 12:58:09 +0200

    compare the user response with solution

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ Spaced Repetition System SQL practice
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# solution_df = duckdb.sql(ANSWER_STR).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
@@ -27,20 +25,33 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+exercise_name = exercise.loc[0, "exercise_name"]
+with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+    answer = f.read()
+
+solution_df = con.execute(answer).df()
+
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
     result = con.execute(query).df()
-    st.dataframe(result)
 
-    # try:
-    #     result = result[solution_df.columns]
-    #     result.compare(solution_df)
-    #     st.markdown("**:green[Bonne réponse]**")
-    # except KeyError:
-    #     st.error("Some columns are missing!")
+    try:
+        result = result[solution_df.columns]
+        result.compare(solution_df)
+        st.markdown("**:green[Bonne réponse]**")
+    except KeyError as e:
+        st.error("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.error(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
+    st.dataframe(result)
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -52,7 +63,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
-        answer = f.read()
     st.write(answer)
