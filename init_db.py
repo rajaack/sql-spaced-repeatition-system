7843ce83918485117ef8568ecf8e79ab6044dd3e
rajaack <jackyrajemison@gmail.com>
2025-04-03 19:43:32 +0200

    init tables size and trademake

--- init_db.py
+++ init_db.py
@@ -21,10 +21,10 @@ con = duckdb.connect(database=PATH_DB_FILE, read_only=False)
 # EXERCISES LIST
 # ------------------------------------------------------------
 data = {
-    "theme": ["cross_joins", "window_functions"],
-    "exercise_name": ["beverages_and_food", "simple_window"],
-    "tables": [["beverages", "food_items"], "simple_window"],
-    "last_reviewed": ["1970-01-01", "1970-01-01"],
+    "theme": ["cross_joins", "cross_joins2", "window_functions"],
+    "exercise_name": ["beverages_and_food", "size_and_trademark", "simple_window"],
+    "tables": [["beverages", "food_items"], ["size", "trademark"], "simple_window"],
+    "last_reviewed": ["1970-01-01", "1970-01-01", "1970-01-01"],
 }
 memory_state_df = pd.DataFrame(data)
 con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
@@ -49,3 +49,23 @@ muffin,3
 """
 food_items = pd.read_csv(io.StringIO(CSV2))
 con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
+
+CSV3 = """
+size
+XS
+M
+L
+XL
+"""
+size = pd.read_csv(io.StringIO(CSV3))
+con.execute("CREATE TABLE IF NOT EXISTS size AS SELECT * FROM size")
+
+CSV4 = """
+trademark
+Nike
+Asphalte
+Abercrombie
+Lewis
+"""
+trademark = pd.read_csv(io.StringIO(CSV4))
+con.execute("CREATE TABLE IF NOT EXISTS trademark AS SELECT * FROM trademark")
