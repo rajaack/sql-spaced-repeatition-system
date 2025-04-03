2081905c10315a6862312915307783b3a8615346
Jacky R <132228904+rajaack@users.noreply.github.com>
2025-04-03 20:23:58 +0200

    Add cross join exercise with sizes and trademaks tables
    * improve the solution display
    
    * init tables size and trademake
    
    * append solution for the exercise
    
    * change the name of tables
    
    * sort exercise with last_reviewed
    
    * quality code

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # pylint: disable=missing-module-docstring
 
-import ast
-
 import duckdb
 import streamlit as st
 
@@ -22,7 +20,12 @@ with st.sidebar:
         placeholder="Select a theme ...",
     )
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    exercise = (
+        con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index()
+    )
     st.write(exercise)
 
 exercise_name = exercise.loc[0, "exercise_name"]
@@ -56,11 +59,11 @@ if query:
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
         st.write(f"table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()
         st.dataframe(df_table)
 
 with tab3:
-    st.write(answer)
+    st.text(answer)
